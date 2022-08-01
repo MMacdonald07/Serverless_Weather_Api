99f10374ddb784145e70970076a0a8a0beb546fd
Matthew <matthew.mmacdonald@gmail.com>
2022-08-01 13:01:22 +0100

    Configured endpoint to return geocoded location based on path

--- index.js
+++ index.js
@@ -10,16 +10,45 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.lambdaHandler = void 0;
+require('dotenv').config();
+// import { forecast } from "./utils/forecast";
+const https = require('https');
 const lambdaHandler = (event) => __awaiter(void 0, void 0, void 0, function* () {
-    let location = '';
-    if (event.pathParameters && event.pathParameters.proxy) {
-        location = event.pathParameters.proxy;
-    }
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            location
-        })
-    };
+    console.log(event);
+    let dataString = '';
+    const response = yield new Promise((resolve, reject) => {
+        const { pathParameters } = event;
+        if (!pathParameters || !pathParameters.proxy) {
+            resolve({
+                statusCode: 400,
+                body: 'Please provide a location in url'
+            });
+        }
+        // @ts-ignore
+        const geoUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${pathParameters.proxy}.json?access_token=${process.env.GEO_KEY}&limit=1`;
+        const req = https.get(geoUrl, function (res) {
+            res.on('data', (chunk) => {
+                dataString += chunk;
+            });
+            res.on('end', () => {
+                const latitude = JSON.parse(dataString).features[0].center[1];
+                const longitude = JSON.parse(dataString).features[0].center[0];
+                resolve({
+                    statusCode: 200,
+                    body: JSON.stringify({
+                        latitude,
+                        longitude
+                    }, null, 4)
+                });
+            });
+        });
+        req.on('error', (err) => {
+            reject({
+                statusCode: 500,
+                body: 'Error in location search'
+            });
+        });
+    });
+    return response;
 });
 exports.lambdaHandler = lambdaHandler;
--- index.ts
+++ index.ts
@@ -1,16 +1,50 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+require('dotenv').config();
+const https = require('https');
+import { APIGatewayProxyEvent } from "aws-lambda";
 
-export const lambdaHandler = async (event : APIGatewayProxyEvent) : Promise<APIGatewayProxyResult> => {
-    let location : String = '';
+export const lambdaHandler = async (event : APIGatewayProxyEvent) => {
+    console.log(event);
+    let dataString : string = '';
+    let location : string = '';
 
-    if (event.pathParameters && event.pathParameters.proxy) {
-        location = event.pathParameters.proxy;
-    }
+    const response = await new Promise((resolve, reject) => {
+        const { pathParameters } = event;
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            location
+        if (!pathParameters || !pathParameters.proxy) {
+            resolve({
+                statusCode: 400,
+                body: 'Please provide a location in url'
+            })
+        } else {
+            location = pathParameters.proxy;
+        }
+
+        const geoUrl: string = `https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?access_token=${process.env.GEO_KEY}&limit=1`;
+        const req = https.get(geoUrl, function (res : any) {
+            res.on('data', (chunk : string) => {
+                dataString += chunk;
+            });
+            res.on('end', () => {
+                const latitude = JSON.parse(dataString).features[0].center[1];
+                const longitude = JSON.parse(dataString).features[0].center[0];
+                resolve({
+                    statusCode: 200,
+                    body: JSON.stringify({
+                        latitude,
+                        longitude
+                    }, null, 4)
+                });
+            });
+        });
+
+        req.on('error', (err : Error) => {
+            console.log(err);
+            reject({
+                statusCode: 500,
+                body: 'Error in location search'
+            })
         })
-    }
+    })
+
+    return response;
 }
